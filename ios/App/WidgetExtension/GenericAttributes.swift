29ce2428b453e3b287313a39e413fa0abb1db9db
Ultimate <63293118+UltimateBoi@users.noreply.github.com>
2026-03-18 10:24:04 +0000

    feat: enable support for Live Activities in iOS and update GenericAttributes structure

--- ios/App/WidgetExtension/GenericAttributes.swift
+++ ios/App/WidgetExtension/GenericAttributes.swift
@@ -2,6 +2,8 @@ import ActivityKit
 import Foundation
 
 struct GenericAttributes: ActivityAttributes {
+    var type: String
+    
     public struct ContentState: Codable, Hashable {
         var title: String
         var subtitle: String
